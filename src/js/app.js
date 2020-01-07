2891d1e0a071a5b5c9197e6a28e735d5c98c8a8f
faizunnabi <faizunnabi1990@gmail.com>
2020-01-07 16:02:38 +0400

    Initial commit

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+require('./styleloader');
+require('../../postcss.config');
+require('../../node_modules/jquery/dist/jquery');
+require('../../node_modules/bootstrap/dist/js/bootstrap.bundle');
+
+$(document).ready(function () {
+    console.log('Working !');
+});
